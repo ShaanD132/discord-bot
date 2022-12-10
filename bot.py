af19ed9606cc5f4ce8cc7b5fcd9e31e1d1a510b6
Shaan <shaanduss@gmail.com>
2022-12-11 02:24:56 +0800

    ok

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import tasks
 import pymongo
 import os
 
-token = os.getenv(DISCORD_TOKEN)
+token = os.getenv("DISCORD_TOKEN")
 
 mongo = pymongo.MongoClient("mongodb+srv://shaand:Sana132@lebbk.urxltwo.mongodb.net/?retryWrites=true&w=majority")
 db = mongo.lebbk
